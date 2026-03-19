b978be58775a4d440d7e3acdab4ce52d7234608d
ANDREI-DAN MASALA <93584418+AndreiM2@users.noreply.github.com>
2026-03-19 21:42:26 +0200

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-def mesaj_ascuns(imagine, mesaj, iesire, key):
+
+def mesaj_ascuns(imagine, mesaj, iesire):
     img = Image.open(imagine)
     if img.mode != 'RGB':
         img = img.convert('RGB')
@@ -11,7 +12,7 @@ def mesaj_ascuns(imagine, mesaj, iesire, key):
 
     i = 0
 
-    mesaj += key
+    mesaj += '###'
     mesaj_binar = ''.join([format(ord(char), '08b') for char in mesaj])
 
     for linie in range(inaltime):
@@ -29,11 +30,48 @@ def mesaj_ascuns(imagine, mesaj, iesire, key):
     copie.save(iesire, format='PNG')
     print(f"Mesaj ascuns in {iesire}!")
 
-if __name__ == "__main__":
-    imagine = input("Introdu numele fișierului de imagine : ")
-    mesaj = input("Introdu mesajul pe care vrei să-l ascunzi: ")
-    iesire = input("Introdu numele fișierului de ieșire : ")
-    key = input("Introdu cheia secreta: ")
 
-    mesaj_ascuns(imagine, mesaj, iesire, key)
-    print("Mesajul a fost introdus cu succes!")
+def reveal_mesaj(imagine):
+    img = Image.open(imagine)
+    mesaj_binar = ""
+    for linie in range(img.height):
+        for coloana in range(img.width):
+            pixel = img.getpixel((coloana, linie))
+            for n in range(3):
+                mesaj_binar += str(pixel[n] & 1)
+
+    bits = [mesaj_binar[i:i + 8] for i in range(0, len(mesaj_binar), 8)]
+    decodat = ''.join([chr(int(b, 2)) for b in bits])
+    return decodat.split('###')[0]
+
+mesaj_ascuns("imagine.png", "Mesaj secret!", "imagine_codificata.png")
+print("Mesaj extras:", reveal_mesaj("imagine_codificata.png"))
+
+def vizualizeaza_diferente(original, codificata, iesire):
+    img_orig = Image.open(original)
+    img_codif = Image.open(codificata)
+
+    if img_orig.size != img_codif.size:
+        print("Imaginile au dimensiuni diferite!")
+        return
+
+    latime, inaltime = img_orig.size
+    viz = Image.new("RGB", (latime, inaltime), "black")
+
+    for linie in range(inaltime):
+        for coloana in range(latime):
+            pixel_orig = img_orig.getpixel((coloana, linie))
+            pixel_codif = img_codif.getpixel((coloana, linie))
+
+            # Dacă există diferență în LSB, pixelul devine alb
+            if any((pixel_orig[i] & 1) != (pixel_codif[i] & 1) for i in range(3)):
+                viz.putpixel((coloana, linie), (255, 255, 255))
+
+    viz.save(iesire)
+    print(f"Imaginea cu diferențe a fost salvată în {iesire}!")
+
+mesaj_ascuns("imagine.png", "Mesaj secret!", "imagine_codificata.png")
+print("Mesaj extras:", reveal_mesaj("imagine_codificata.png"))
+vizualizeaza_diferente("imagine.png", "imagine_codificata.png", "diferente.png")
+
+
